9619779f1395c2da92be72119d612018f4164195
Your Name <styvesdaudet@gmail.com>
2024-12-09 09:58:17 +0100

    home

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./hospital.db', (err) => {
+const db = new sqlite3.Database('./../hospital.db', (err) => {
     if (err) {
         console.error('Erreur lors de la connexion à la base de données :', err.message);
     } else {
